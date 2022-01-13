585ce5274b1e9824ef8ec013d8c2459d45bfd314
Richard Rosalion <richard@mbp2015.home.rosalion.net>
2022-01-13 22:00:15 +1100

    Wait for postgress before starting backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,6 +2,13 @@
 export PYTHONUNBUFFERED=TRUE
 export PYTHONFAULTHANDLER=1
 mkdir -p /logs
+
+until python -c "import os; from django.db import connection; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ownphotos.settings'); print(connection.cursor().execute('select * from pg_catalog.pg_tables;'))" 2> /dev/null
+do
+  echo "Waiting for database..."
+  sleep 2
+done
+
 python image_similarity/main.py 2>&1 | tee /logs/gunicorn_image_similarity.log &
 python manage.py showmigrations | tee /logs/show_migrate.log
 python manage.py migrate | tee /logs/command_migrate.log
